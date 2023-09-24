5f599be203783be6c14858a44690a0639c0b7685
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-09-24 03:31:30 -0700

    Fix functopn to have it assign the index of j to the array instead of i

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,11 +18,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 			}
 		}
 		print_array(array, size);
