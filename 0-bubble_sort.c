44495b13f22acd208fd7e15658c1f2f20a760390
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-09-24 04:10:36 -0700

    Fix documentation and add a second condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,8 +1,8 @@
 #include "sort.h"
 #include <stddef.h>
 
-/*
- * bubble_sort - sorts an array in ascending order with bubble sort.
+/**
+ * bubble_sort - Sorts an array in ascending order with bubble sort.
  *
  * @array: array to be iterated through
  * @size: number of elements in @array
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j + 1])
+			if ((array[j] > array[j + 1]) && (array[j] > array[j - 1]))
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
