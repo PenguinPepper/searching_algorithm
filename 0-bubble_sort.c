a7a7ef05e5528ea0e5065df01524094fea5b0d64
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-09-24 03:25:35 -0700

    Fix the second loop condition to have it be les than size -1 - i

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,22 +2,23 @@
 #include <stddef.h>
 
 /*
- * bubble_sort - sorts an array of integers
- * in ascending order using bubble sort.
+ * bubble_sort - sorts an array in ascending order with bubble sort.
  *
  * @array: array to be iterated through
  * @size: number of elements in @array
+ *
  */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
 
-	for(i= 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for(j = 0; j <size -1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if(array[i] > array[i + 1])
+			if (array[i] > array[i + 1])
 			{
 				temp = array[i];
 				array[i] = array[i + 1];
