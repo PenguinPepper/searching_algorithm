568c95420e77dba064b9c4b511e911608eff1ec0
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-09-24 03:39:29 -0700

    Print the array after every swap

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,9 +14,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; ++i)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; ++j)
 		{
 			if (array[j] > array[j + 1])
 			{
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 			}
+			print_array(array, size);
 		}
-		print_array(array, size);
 	}
 }
