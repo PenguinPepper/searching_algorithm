85c44ad03110fa4e7218982c1677ad14060acc31
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-09-24 04:22:09 -0700

    Add the time complexity of insertion sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,13 +15,13 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 
 	if (size == 2)
-		print_array(array,size);
+		print_array(array, size);
 
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if ((array[j] > array[j + 1]) && (array[j] > array[j - 1]))
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
