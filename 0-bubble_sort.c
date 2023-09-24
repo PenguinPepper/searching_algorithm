effb8242e2ab8bac3a1ddb4173ba8db3e938ef20
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-09-24 04:15:15 -0700

    Add condtiton for when the size is less than 2

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,6 +14,9 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	int temp;
 
+	if (size == 2)
+		print_array(array,size);
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
